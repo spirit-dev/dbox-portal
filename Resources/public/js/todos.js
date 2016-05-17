4881416f5d8270b0d582db56afe2e82f88054e2f
Spirit-Dev <bordat.jean@gmail.com>
2016-05-17 21:18:31 +0200

    Adding signature

--- Resources/public/js/todos.js
+++ Resources/public/js/todos.js
@@ -1,3 +1,23 @@
+/*
+ * Copyright (c) 2016. Spirit-Dev
+ * Licensed under GPLv3 GNU License - http://www.gnu.org/licenses/gpl-3.0.html
+ *    _             _
+ *   /_`_  ._._/___/ | _
+ * . _//_//// /   /_.'/_'|/
+ *    /
+ *    
+ * Since 2K10 until today
+ *  
+ * Hex            53 70 69 72 69 74 2d 44 65 76
+ *  
+ * By             Jean Bordat
+ * Twitter        @Ji_Bay_
+ * Mail           <bordat.jean@gmail.com>
+ *  
+ * File           todos.js
+ * Updated the    17/05/16 21:18
+ */
+
 $(document).ready(function () {
 
     $('#sendTodos').click(function () {
