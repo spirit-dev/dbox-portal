2b69772d059fd59ab086604a9c077dc24ac93460
Jean Bordat <bordat.jean@gmail.com>
2016-06-06 16:01:51 +0200

    Updating service name call

--- API/GitLabAPICore.php
+++ API/GitLabAPICore.php
@@ -6,17 +6,17 @@
  *   /_`_  ._._/___/ | _
  * . _//_//// /   /_.'/_'|/
  *    /
- *
+ *  
  * Since 2K10 until today
- *
+ *  
  * Hex            53 70 69 72 69 74 2d 44 65 76
- *
+ *  
  * By             Jean Bordat
  * Twitter        @Ji_Bay_
  * Mail           <bordat.jean@gmail.com>
- *
+ *  
  * File           GitLabAPICore.php
- * Updated the    16/05/16 12:28
+ * Updated the    06/06/16 16:01
  */
 
 namespace SpiritDev\Bundle\DBoxPortalBundle\API;
@@ -71,7 +71,7 @@ abstract class GitLabAPICore {
         // Applying API connection info
         $this->gitLabUrl = $container->getParameter("spirit_dev_d_box_portal.gitlab_api.url");
         $this->gitLabToken = $container->getParameter("spirit_dev_d_box_portal.gitlab_api.token");
-        $this->ldapProvider = $container->getParameter("spirit_dev_d_box_portal.ldap_driver.provider");
+        $this->ldapProvider = $container->getParameter("spirit_dev_d_box_user.ldap_driver.provider");
         // Creating client
         $this->gitLabClient = $this->authenticate();
     }
